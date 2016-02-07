94d5da9bd6dd8a305704350952366364486f1625
dotlouis <1891109+dotlouis@users.noreply.github.com>
2016-02-07 23:31:13 +0100

    Added environment variables and default

--- src/env.js
+++ src/env.js
@@ -1,5 +1,11 @@
 import pkg from '../package.json';
 
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+
+export const APP_HOST = process.env.APP_HOST || '0.0.0.0';
 export const APP_PORT = process.env.APP_PORT || '8080';
+export const APP_NAME = process.env.APP_NAME || pkg.name;
+
+export const API_ROOT = process.env.API_ROOT || '/api';
 export const API_VERSION = process.env.API_VERSION || pkg.version.split('.').shift();
-export const API_PATH = process.env.API_PATH || `/api/v${API_VERSION}`;
+export const API_PATH = process.env.API_PATH || `${API_ROOT}/v${API_VERSION}`;
