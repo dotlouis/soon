8cc49035ed1c3d68618c51d7b76e6e7f8f1904ac
dotlouis <1891109+dotlouis@users.noreply.github.com>
2016-02-07 23:58:21 +0100

    Added logging helper to simplify debugging statements

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -3,11 +3,21 @@ import bunyan from 'bunyan';
 import bunyanRequest from 'express-bunyan-logger';
 
 const logger = bunyan.createLogger({ name: ENV.APP_NAME });
+
+// Enable debug-level logging while in development
+if(ENV.NODE_ENV === 'development'){
+	logger.level(bunyan.DEBUG);
+}
+
+// helper to simlpify debugging statements throught the app.
+const debug = (obj) => logger.debug({debug:obj});
+
+// A middleware that will log requests
 const requestLogger = bunyanRequest({
 	logger: logger,
 	// excludes all properties to get a clean morgan-like output
 	excludes: ['*']
 });
 
-export {requestLogger};
+export {requestLogger, debug};
 export default logger;
