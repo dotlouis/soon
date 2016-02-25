aa25a9d68466cd8670f16039384872343c26a64e
dotlouis <1891109+dotlouis@users.noreply.github.com>
2016-02-25 19:09:58 +0100

    Added REST endpoints for basic CRUD operation
    - getAll
    - getById
    - deleteById
    - create

--- src/event/event.controller.js
+++ src/event/event.controller.js
@@ -4,12 +4,79 @@ import {wrap, delay} from '../utils/utils';
 
 let router = express.Router();
 
-router.route('/')
-.post(wrap(async(req, res)=>{
-	let e = new Event(req.body);
-	await e.save();
-	// await delay(1000, 'booo', true);
-	res.send(e);
+router.route('/:id')
+// get by id
+.get(wrap(async(req, res)=>{
+	try{
+		let event = await Event.findById(req.params.id).exec();
+		if(!event)
+			res.sendStatus(404);
+		else
+			res.json(event);
+	}
+	catch(err){
+		switch(err.name){
+			// wrong id format
+			case 'CastError':
+				req.log.warn(err);
+				res.sendStatus(400);
+				break;
+		default:
+			throw err;
+		}
+	}
+}))
+// delete by id
+.delete(wrap(async(req,res)=>{
+	try{
+		let event = await Event.findByIdAndRemove(req.params.id).exec();
+		if(!event)
+			res.sendStatus(404);
+		else
+			res.sendStatus(200);
+	}
+	catch(err){
+		switch(err.name){
+			// wrong id format
+			case 'CastError':
+				req.log.warn(err);
+				res.sendStatus(400);
+				break;
+		default:
+			throw err;
+		}
+	}
 }));
+
+router.route('/')
+// get all
+.get(wrap(async(req, res)=>{
+	let events = await Event.find();
+	res.json(events);
+}))
+// create
+.post(wrap(async(req, res)=>{
+	try {
+		let event = new Event(req.body);
+		await event.save();
+		res.json(event);
+	}
+	catch(err){
+		switch(err.name){
+			// wrong params format
+			case 'ValidationError':
+				req.log.warn(err);
+				res.sendStatus(400);
+				break;
+		default:
+			throw err;
+		}
+	}
+}));
+
+/* TODO
+¨* - HEAD (check if a record exists)
+ * - PUT/PATCH (update a record)
+ */
 
 export default router;
