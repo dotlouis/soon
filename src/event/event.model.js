7c097a3cfa6b45a231f0c046a6a8321ff5ed4133
dotlouis <1891109+dotlouis@users.noreply.github.com>
2016-02-26 15:32:05 +0100

    Moved time computation out of constructor into own function

--- src/event/event.model.js
+++ src/event/event.model.js
@@ -9,36 +9,44 @@ class Event{
 	static schema = Object.assign({
 		start: { type: Date, required: true },
 		end: { type: Date, required: true },
-		duration: { type: String }
+		duration: { type: String, required: true }
 	}, defaultSchema);
 
 	// This method act as a constructor
 	// Used to process complex params default
 	static spawn(params = {}){
-		let opt = {};
-		opt.start = moment(params.start) || moment();
-
-		// end parameter take precedence over duration
-		if(params.end){
-			opt.end = moment(params.end);
-			opt.duration = moment.duration(opt.end.diff(opt.start));
-		}
-
-		// you can specify a duration rather than an end
-		else if(params.duration){
-			opt.duration = moment.duration(params.duration);
-			opt.end = moment(opt.start).add(opt.duration);
-		}
-
-		// if none are specified, the default event is 1h
-		else{
-			opt.duration = moment.duration(1, 'h');
-			opt.end = moment(opt.start).add(opt.duration);
-		}
+		let event = Object.assign({},
+			computeTimes(params) // start, end, duration
+		);
 
 		// "real" constructor with params as defined in the schema
-		return (new this(opt));
+		return (new this(event));
 	}
+}
+
+function computeTimes(params){
+	let start, end, duration;
+	start = moment(params.start) || moment();
+
+	// end parameter take precedence over duration
+	if(params.end){
+		end = moment(params.end);
+		duration = moment.duration(end.diff(start));
+	}
+
+	// you can specify a duration rather than an end
+	else if(params.duration){
+		duration = moment.duration(params.duration);
+		end = moment(start).add(duration);
+	}
+
+	// if none are specified, the default event is 1h
+	else{
+		duration = moment.duration(1, 'h');
+		end = moment(start).add(duration);
+	}
+
+	return { start, end, duration };
 }
 
 // Allows to call `new Event()` when importing this file
