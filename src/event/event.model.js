7332a7d9018a5f63af74dfbbcbb44db6b60ba022
dotlouis <1891109+dotlouis@users.noreply.github.com>
2016-03-20 15:08:53 +0100

    Wrapped the date validation hook in try catch

--- src/event/event.model.js
+++ src/event/event.model.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import {BadRequest} from '../errors/errors';
 import {ObjectId, defaultSchema} from '../mongoose/mongoose';
 import {Model, pre, post} from 'mongoose-model-decorators';
 
@@ -15,30 +16,38 @@ class Event{
 
 	@pre('validate')
 	computeTimes(next){
-		let start, end, duration;
-		start = moment(this.start) || moment();
+		try{
+			let start, end, duration;
+			start = moment(this.start) || moment();
 
-		// end parameter take precedence over duration
-		if(this.end){
-			end = moment(this.end);
-			duration = moment.duration(end.diff(start));
+			// end parameter take precedence over duration
+			if(this.end){
+				end = moment(this.end);
+				duration = moment.duration(end.diff(start));
+			}
+
+			// you can specify a duration rather than an end
+			else if(this.duration){
+				duration = moment.duration(this.duration);
+				end = moment(start).add(duration);
+			}
+
+			// if none are specified, the default event is 1h
+			else{
+				duration = moment.duration(1, 'h');
+				end = moment(start).add(duration);
+			}
+
+			this.start = start;
+			this.end = end;
+			this.duration = duration;
 		}
-
-		// you can specify a duration rather than an end
-		else if(this.duration){
-			duration = moment.duration(this.duration);
-			end = moment(start).add(duration);
+		catch(err){
+			throw new BadRequest('Wrong start and/or end and/or duration format');
 		}
+		next();
+	}
 
-		// if none are specified, the default event is 1h
-		else{
-			duration = moment.duration(1, 'h');
-			end = moment(start).add(duration);
-		}
-
-		this.start = start;
-		this.end = end;
-		this.duration = duration;
 		next();
 	}
 
