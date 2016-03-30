d0ddb3c1ee0e4fbda4a437398eb5472e48f4a523
dotlouis <1891109+dotlouis@users.noreply.github.com>
2016-03-31 01:26:04 +0200

    Event model now uses RRule schema as subdocument
    RRule dtstart is defaulted to the event start date

--- src/event/event.model.js
+++ src/event/event.model.js
@@ -1,6 +1,6 @@
 import range from '../range/range';
 import {BadRequest} from '../errors/errors';
-import {RRule, RRuleSet, rrulestr} from 'rrule';
+import RRule from '../rrule/rrule.schema';
 import {ObjectId, defaultSchema} from '../mongoose/mongoose';
 import {Model, pre, post} from 'mongoose-model-decorators';
 
@@ -9,12 +9,13 @@ class Event{
 
 	// inherit the default mongoose schema
 	static schema = Object.assign({
-		chain: { type: ObjectId, ref:'Chain', required: true, private: true },	// the chain the event is linked to
+		// the chain the event is linked to
+		chain: { type: ObjectId, ref: 'Chain', required: true, private: true },
 		start: { type: Date, required: true },
 		end: { type: Date, required: true },
 		duration: { type: String, required: true },
-		rrule: [{ type: String }],	// the recurrence rule to generate events from
-		virtual: { type: Boolean }	// the event has been virtualDate by a recurrence rule
+		// the recurrence rule to generate events from
+		rrule: RRule
 	}, defaultSchema);
 
 	@pre('validate')
@@ -42,89 +43,9 @@ class Event{
 	}
 
 	@pre('validate')
-	computeRRule(next){
-		let rruleSet;
-		try{
-			if(typeof this.rrule === 'string'){
-				// we parse the string to check for errors and create a rruleset
-				rruleSet = rrulestr(this.rrule, {forceset: true});
-				// we convert back the rruleset to its array representation
-				this.rrule = rruleSet.valueOf();
-			}
-		}
-		catch(err){
-			throw new BadRequest('Wrong rrule format');
-		}
+	computeRRuleDefault(next){
+		this.rrule._dtstart = this.start;
 		next();
-	}
-
-	// return and persist events generated from a recurrence rule
-	generateVirtuals(params = {}){
-		try{
-			// if no rrule we don't do anything
-			if(!this.rrule || this.rrule.length === 0)
-				return null;
-
-			let self = this,
-				// default range start date is now
-				after = moment(),
-				// default range end date is 1 month after the range start date
-				before = moment(after).add(1, 'months'),
-				virtualEvents = [],
-				// we convert the array of rrule into its string representation
-				// see https://github.com/jkbrzt/rrule#rrulestr-function
-				rruleSetString = self.rrule.join('\n'),
-				// we create the rruleSet object from the string
-				rruleSet = rrulestr(rruleSetString, {forceset: true}),
-				// the maximum number of virtual events to be created/returned
-				// even if the COUNT of the RRule is greater or if the number of
-				// occurences between the after and before date should generate more
-				// dates (to be refined after some tests)
-				MAX_VIRTUALS = 100,
-				INCLUSIVE = false;
-
-			// for each rrule in rruleset we set the dtstart and weekday
-			// to the start date of the event. Therefore any original RRULE
-			// dtstart and byweekday parameter will be overriden
-			let weekday = moment(self.start).isoWeekday() - 1;
-			for(let rrule of rruleSet._rrule){
-				rrule.options.dtstart = self.start;
-				rrule.options.byweekday = [ weekday ];
-			}
-
-			// if there are params, we override defaults
-			if(params.after)
-				after = moment(params.after);
-			if(params.before)
-				before = moment(params.before);
-
-			rruleSet.between(after.toDate(), before.toDate(), INCLUSIVE, (date, i)=>{
-				if(i > MAX_VIRTUALS)
-					return false;
-
-				// original event is not included because it already exist in DB
-				// how convenient: https://github.com/jkbrzt/rrule/issues/84
-
-				virtualEvents.push({
-					start: date,
-					// we use duration instead of end parameter
-					// in case the event is more than 24 hours long
-					duration: self.duration,
-					rrule: self.rrule,
-					chain: self.chain,
-					// the virtual flag let us know the event has been
-					// auto-generated from a rrule
-					virtual: true
-				});
-
-				return true;
-			});
-
-			return virtualEvents;
-		}
-		catch(err){
-			throw err;
-		}
 	}
 
 	addTo(chain){
