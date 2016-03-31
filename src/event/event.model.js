9a03ab2e6ff04703839414d207b0cd300f5363e5
dotlouis <1891109+dotlouis@users.noreply.github.com>
2016-03-31 21:18:19 +0200

    Models dont throw Request Errors anymore
    it's the controller job to catch and create Client Errors

--- src/event/event.model.js
+++ src/event/event.model.js
@@ -1,6 +1,5 @@
 import range from '../range/range';
-import {BadRequest} from '../errors/errors';
-import RRule from '../rrule/rrule.schema';
+import RRuleSchema from '../rrule/rrule.schema';
 import {ObjectId, defaultSchema} from '../mongoose/mongoose';
 import {Model, pre, post} from 'mongoose-model-decorators';
 
@@ -15,30 +14,19 @@ class Event{
 		end: { type: Date, required: true },
 		duration: { type: String, required: true },
 		// the recurrence rule to generate events from
-		rrule: RRule
+		rrule: RRuleSchema
 	}, defaultSchema);
 
 	@pre('validate')
 	computeTimes(next){
-		try{
-			let {start, end, duration} = range({
-				rangeLength: 1,
-				rangeUnit: 'hours'
-			})(this.start, this.end, this.duration);
+		let {start, end, duration} = range({
+			rangeLength: 1,
+			rangeUnit: 'hours'
+		})(this.start, this.end, this.duration);
 
-			this.start = start;
-			this.end = end;
-			this.duration = duration;
-		}
-		catch(err){
-			switch(err.name){
-				case 'RangeError':
-					throw err;
-					break;
-				default:
-					throw new BadRequest('Wrong start and/or end and/or duration format');
-			}
-		}
+		this.start = start;
+		this.end = end;
+		this.duration = duration;
 		next();
 	}
 
